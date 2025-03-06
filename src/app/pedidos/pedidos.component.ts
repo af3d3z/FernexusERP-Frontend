1d1c309dfddafa6822cee211474698e9d2b91be5
nestorss05 <nestor.sanchez@iesnervion.es>
2025-03-06 13:34:53 +0100

    Tabla falsa con push

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import {AppMenuComponent} from '../app-menu/app-menu.component';
-import {CurrencyPipe, NgClass, NgFor} from '@angular/common';
+import {CurrencyPipe, NgClass, NgFor, CommonModule} from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pedidos',
   standalone: true,
-  imports: [NgFor, AppMenuComponent, CurrencyPipe, NgClass],
+  imports: [CommonModule, NgFor, AppMenuComponent, CurrencyPipe, NgClass, ReactiveFormsModule],
   templateUrl: './pedidos.component.html',
   styleUrl: './pedidos.component.scss'
 })
 export class PedidosComponent {
   botonColor: string = '#011640';
+  mostrarModal: boolean = false;
+  modalBackgroundColor: string = '#233a59';
 
   pedidos = [
     { proveedor: 'Proveedor A', producto: 'Producto X', cantidad: 100, precio: 15.00, fechaPedido: '2025-03-01', estado: 'Pendiente' },
     { proveedor: 'Proveedor B', producto: 'Producto Y', cantidad: 50, precio: 25.00, fechaPedido: '2025-03-02', estado: 'Completado' },
     { proveedor: 'Proveedor C', producto: 'Producto Z', cantidad: 200, precio: 10.00, fechaPedido: '2025-03-03', estado: 'Cancelado' }
   ];
+
+  pedidoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.pedidoForm = this.fb.group({
+      proveedor: ['', Validators.required],
+      producto: ['', Validators.required],
+      cantidad: [1, [Validators.required, Validators.min(1)]],
+      precio: [0, [Validators.required, Validators.min(0.01)]],
+      fechaPedido: [new Date().toISOString().split('T')[0], Validators.required],
+      estado: ['Pendiente', Validators.required]
+    });
+  }
 
   getEstadoClass(estado: string): string {
     switch (estado) {
@@ -28,7 +44,18 @@ export class PedidosComponent {
   }
 
   agregarPedido() {
-    console.log("Agregar nuevo pedido");
-    // TODO: modal para formulario o pagina nueva
+    this.mostrarModal = true; // Muestra el modal
   }
+
+  cerrarModal() {
+    this.mostrarModal = false; // Oculta el modal
+  }
+
+  guardarPedido() {
+    if (this.pedidoForm.valid) {
+      this.pedidos.push(this.pedidoForm.value);
+      this.cerrarModal();
+    }
+  }
+
 }
